759e59a263dede2c0236d92b5514636ff6dea9c2
Robert Polevoi <robert.polevoi@gmail.com>
2016-09-25 16:19:52 -0700

    Create route_b.component.ts

---
+++ app/route_b/route_b.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  template: `<h1>Now it's the Route B view!</h1>`,
+  styleUrls: ['route_b.component.css']
+})
+export class RouteBComponent {
+
+}
