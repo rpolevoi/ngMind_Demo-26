7596d34d8cf6ce6af323fb37bcc73856b97657e5
Robert Polevoi <robert.polevoi@gmail.com>
2016-09-28 16:33:19 -0700

    Update app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule }  from '@angular/platform-browser';
 
 import { AppComponent }   from './app.component';
 import { RouteAModule }   from './route_a/route_a.module';
-//import { routing, appRoutingProviders }  from './app.routing';
 import { routing }  from './app.routing';
 
 @NgModule({
